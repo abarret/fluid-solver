dcf9edb4e53536b7e346c934025ba6185027a500
Aaron Barrett <abarret@live.unc.edu>
2017-11-02 16:31:36 -0400

    Added function to interpolate data. For multigrid solver.

---
+++ interpolate.m
@@ -0,0 +1,26 @@
+function u_new = interpolate(u,ratio,dx,dy)
+% Assume ratio is 2 for now
+% Assume u is cell centered
+[r,c] = size(u);
+u_new = zeros(r*2,c*2);
+u_temp = zeros(r*2+2,c*2);
+u = fillBoundariesCenter(u,1);
+% Interpolate in one dimension...
+for i = 1:length(u(:,1))
+    u_temp(:,i) = interpolate1D(u(:,i),ratio,dx,dy);
+end
+% Interpolate in other dimension...
+for i = 1:length(u_temp(1,:))
+    u_new(:,i) = interpolate1D(u_temp(:,i),ratio,dx,dy);
+end
+end
+
+function u_new = interpolate1D(u,ratio,dx,dy)
+% Assume ratio is 2 for now... Better interpolations can come later
+% Assume u is cell centered for now....
+% This only works for one spatial dimension
+u_new = zeros(1,length(u(2:end-1))*2);
+%u = [u(end) u u(1)];
+u_new(2:2:end) = 0.25*u(3:end)+0.75*u(2:end-1);
+u_new(1:2:end) = 0.75*u(2:end-1)+0.25*u(1:end-2);
+end
---
+++ test/test_interpolate.m
@@ -0,0 +1,24 @@
+clear; close all;
+addpath('..');
+
+f = @(x,y) sin(pi*x).*sin(pi*y);
+L = 2;
+N = 64; dx = L/N;
+x_side = 0:dx:L;
+y_side = 0:dx:L;
+x = 0.5*(x_side(2:end)+x_side(1:end-1));
+y = 0.5*(y_side(2:end)+y_side(1:end-1));
+[xx,yy] = meshgrid(x,y);
+x_fine_s = 0:dx/2:L; y_fine_s = 0:dx/2:L;
+x_fine = 0.5*(x_fine_s(2:end)+x_fine_s(1:end-1));
+y_fine = 0.5*(y_fine_s(2:end)+y_fine_s(1:end-1));
+[xx_fine,yy_fine] = meshgrid(x_fine,y_fine);
+f_vals = f(xx,yy);
+
+f_vals_fine = interpolate(f_vals,2,0,0);
+figure(1); clf;
+surf(xx,yy,f_vals,'edgecolor','none');
+figure(2); clf;
+surf(xx_fine,yy_fine,f_vals_fine,'edgecolor','none');
+figure(3); clf;
+surf(xx_fine,yy_fine,abs(f_vals_fine-f(xx_fine,yy_fine)),'edgecolor','none');
